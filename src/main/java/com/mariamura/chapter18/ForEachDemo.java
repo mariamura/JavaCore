94f0c2f37c622eb219cbe35de01b980f61f09d14
Maria M <muratowa49@gmail.com>
2021-06-02 19:01:02 +0300

    Updated

--- src/main/java/com/mariamura/chapter18/ForEachDemo.java
+++ src/main/java/com/mariamura/chapter18/ForEachDemo.java
@@ -1,7 +1,26 @@
 package com.mariamura.chapter18;
 
+import java.util.ArrayList;
+
 public class ForEachDemo {
     public static void main(String[] args) {
+        ArrayList<Integer> vals = new ArrayList<>();
+
+        for (int i = 1; i<6; i++) vals.add(i);
+
+        System.out.print("Initial vals content: ");
+        for (int i : vals) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        int sum = 0;
+        for (int i: vals) sum+=i;
+        System.out.println("Sum of vals elements: " + sum);
+
+
+
 
     }
 }
