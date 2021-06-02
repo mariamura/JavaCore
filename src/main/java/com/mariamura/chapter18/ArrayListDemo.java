fa9ed3e4616235bc7e94f649c8b111afd392ad60
Maria M <muratowa49@gmail.com>
2021-06-02 14:06:10 +0300

    Updated

--- src/main/java/com/mariamura/chapter18/ArrayListDemo.java
+++ src/main/java/com/mariamura/chapter18/ArrayListDemo.java
@@ -1,7 +1,37 @@
 package com.mariamura.chapter18;
 
+
+
+
+import java.util.ArrayList;
+
 public class ArrayListDemo {
     public static void main(String[] args) {
+        //create ArrayList
+        ArrayList<String> al = new ArrayList<>();
+
+        System.out.println("ArrayList al was created. Initial al size: " + al.size());
+
+        //add elements
+        al.add("C");
+        al.add("A");
+        al.add("E");
+        al.add("B");
+        al.add("D");
+        al.add("F");
+        al.add(1, "A2");
+
+        System.out.println("al size after elements added: " + al.size());
+
+        //arraylist content
+        System.out.println("al content: " + al);
+
+        //delete elements
+        al.remove("F");
+        al.remove(2);
+
+        System.out.println("al size after elements deleted: " + al.size());
+        System.out.println("al content: " + al);
 
     }
 }
--- src/main/java/com/mariamura/chapter18/ArrayListToArray.java
+++ src/main/java/com/mariamura/chapter18/ArrayListToArray.java
@@ -3,5 +3,6 @@ package com.mariamura.chapter18;
 public class ArrayListToArray {
     public static void main(String[] args) {
 
+
     }
 }
