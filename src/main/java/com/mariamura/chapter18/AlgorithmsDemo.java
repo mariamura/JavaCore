888ca264d36a6f241c0d86ef21f2df195adcbbcd
Maria M <muratowa49@gmail.com>
2021-06-09 17:17:28 +0300

    Updated

--- src/main/java/com/mariamura/chapter18/AlgorithmsDemo.java
+++ src/main/java/com/mariamura/chapter18/AlgorithmsDemo.java
@@ -1,7 +1,40 @@
 package com.mariamura.chapter18;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+
 public class AlgorithmsDemo {
     public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+
+        ll.add(-8);
+        ll.add(20);
+        ll.add(-20);
+        ll.add(8);
+
+        Comparator<Integer> r = Collections.reverseOrder();
+
+        Collections.sort(ll, r);
+        //ll.sort(r) could be used instead
+
+        System.out.print("ll content in reverse order: ");
+        for (int i : ll) System.out.print(i + " ");
+
+        System.out.println();
+
+        Collections.shuffle(ll);
+        System.out.print("ll content after shuffle: ");
+        for (int i : ll) System.out.print(i + " ");
+
+        System.out.println();
+
+        System.out.println("max element " + Collections.max(ll));
+        System.out.println("min element " + Collections.min(ll));
+
+
+
+
 
     }
 }
