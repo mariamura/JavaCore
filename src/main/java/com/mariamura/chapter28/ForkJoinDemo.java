f3b5dd43e20558008a2496e69671e0c707fc9630
Maria M <muratowa49@gmail.com>
2021-07-23 19:56:00 +0300

    Updated

---
+++ src/main/java/com/mariamura/chapter28/ForkJoinDemo.java
@@ -0,0 +1,46 @@
+package com.mariamura.chapter28;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+class SqrtTransform extends RecursiveAction {
+    final int seqTr=1000;
+
+    double[] data;
+    int start, end;
+
+    public SqrtTransform(double[] data, int start, int end) {
+        this.data = data;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected void compute() {
+        if((end-start)<seqTr) {
+            for(int i = start; i<end; i++) {
+                data[i] = Math.sqrt(data[i]);
+            }
+        } else {
+            int middle = (start+end)/2;
+            invokeAll(new SqrtTransform(data, start, middle),
+                      new SqrtTransform(data, middle, end));
+        }
+    }
+}
+
+public class ForkJoinDemo {
+    public static void main(String[] args) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        double[] nums = new double[100000];
+
+        for(int i = 0; i<nums.length; i++) {
+            nums[i] = (double) i;
+        }
+
+        System.out.println("Part initial array..");
+        for(int i = 0; i<10; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
